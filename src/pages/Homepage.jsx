504f58f79fa70ad92a72193dc7a65b27c8474eec
IamKingVictor <kingvictor306@gmail.com>
2024-07-20 20:02:46 +0100

    HeroSection

--- src/pages/Homepage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainLayout from "../components/MainLayout";
-
-export const Homepage = () => {
-  return <MainLayout></MainLayout>;
-};
-
-export default Homepage;
---
+++ src/pages/home/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../conatiner/Hero";
+import MainLayout from "../../components/MainLayout";
+
+export const Homepage = () => {
+  return (
+    <MainLayout>
+      <Hero />
+    </MainLayout>
+  );
+};
+
+export default Homepage;
