00dea4f78e2301c6b5952ebb6dcea7c7eb2359e5
IamKingVictor <kingvictor306@gmail.com>
2024-08-01 20:07:30 +0100

    update

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "../conatiner/Hero";
 import MainLayout from "../../components/MainLayout";
+import Article from "../conatiner/Article";
+import CTA from "../conatiner/CTA";
 
 export const Homepage = () => {
   return (
     <MainLayout>
       <Hero />
+      <Article />
+      <CTA />
     </MainLayout>
   );
 };
